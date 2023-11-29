ae672c7566516b6da5c654208333db3b4e2c8c68
Fredrik Jansson <fj@avassa.io>
2023-11-29 19:12:23 +0100

    Better names.

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,31 @@ use std::env;
 pub type Input<'a> = &'a str;
 pub type PResult<'a, T> = nom::IResult<Input<'a>, T, nom::error::VerboseError<Input<'a>>>;
 
-mod aoc1;
-mod aoc10;
-mod aoc11;
-mod aoc12;
-mod aoc13;
-mod aoc14;
-mod aoc15;
-mod aoc16;
-mod aoc17;
-mod aoc18;
-mod aoc19;
-mod aoc2;
-mod aoc20;
-mod aoc21;
-mod aoc22;
-mod aoc23;
-mod aoc24;
-mod aoc25;
-mod aoc3;
-mod aoc4;
-mod aoc5;
-mod aoc6;
-mod aoc7;
-mod aoc8;
-mod aoc9;
+mod day1;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day2;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
+mod day25;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
 
 fn main() -> anyhow::Result<()> {
     let mut a = env::args();
@@ -36,31 +36,31 @@ fn main() -> anyhow::Result<()> {
     let day = a.next().and_then(|s| s.parse::<usize>().ok()).unwrap_or(0);
 
     match day {
-        1 => aoc1::run(),
-        2 => aoc2::run(),
-        3 => aoc3::run(),
-        4 => aoc4::run(),
-        5 => aoc5::run(),
-        6 => aoc6::run(),
-        7 => aoc7::run(),
-        8 => aoc8::run(),
-        9 => aoc9::run(),
-        10 => aoc10::run(),
-        11 => aoc11::run(),
-        12 => aoc12::run(),
-        13 => aoc13::run(),
-        14 => aoc14::run(),
-        15 => aoc15::run(),
-        16 => aoc16::run(),
-        17 => aoc17::run(),
-        18 => aoc18::run(),
-        19 => aoc19::run(),
-        20 => aoc20::run(),
-        21 => aoc21::run(),
-        22 => aoc22::run(),
-        23 => aoc23::run(),
-        24 => aoc24::run(),
-        25 => aoc25::run(),
+        1 => day1::run(),
+        2 => day2::run(),
+        3 => day3::run(),
+        4 => day4::run(),
+        5 => day5::run(),
+        6 => day6::run(),
+        7 => day7::run(),
+        8 => day8::run(),
+        9 => day9::run(),
+        10 => day10::run(),
+        11 => day11::run(),
+        12 => day12::run(),
+        13 => day13::run(),
+        14 => day14::run(),
+        15 => day15::run(),
+        16 => day16::run(),
+        17 => day17::run(),
+        18 => day18::run(),
+        19 => day19::run(),
+        20 => day20::run(),
+        21 => day21::run(),
+        22 => day22::run(),
+        23 => day23::run(),
+        24 => day24::run(),
+        25 => day25::run(),
         _ => Err(anyhow::anyhow!("Not yet implemented")),
     }
 }
