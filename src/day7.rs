f02bf29aca665f9246c82e98594e4b66c183299f
Fredrik Jansson <fj@avassa.io>
2023-12-08 14:26:43 +0100

    Day 8

--- src/day7.rs
+++ src/day7.rs
@@ -1,3 +1,5 @@
+use std::{cmp::Ordering, collections::HashMap, usize};
+
 pub fn run() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("day7.txt")?;
 
@@ -7,21 +9,263 @@ pub fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn run_1(_input: &str) -> anyhow::Result<usize> {
-  todo!()
+fn run_1(input: &str) -> anyhow::Result<usize> {
+    let mut hands = parse(input);
+
+    hands.sort();
+
+    Ok(hands
+        .iter()
+        .enumerate()
+        .map(|(rank, h)| (rank + 1) * h.bid)
+        .sum())
 }
 
-fn run_2(_input: &str) -> anyhow::Result<usize> {
-  todo!()
+// 252829329 - too low
+// 252884460 - too low
+// 252941412 - too low
+fn run_2(input: &str) -> anyhow::Result<usize> {
+    let mut hands = parse(input);
+
+    const JOKER: Card = Card(11);
+    // First convert jokers to Card(1), to be low worth
+    for hand in hands.iter_mut() {
+        for card in hand.cards.iter_mut() {
+            if *card == JOKER {
+                *card = Card(1);
+            }
+        }
+        hand.t = Hand::calculate_type(&hand.cards);
+    }
+
+    for hand in hands.iter_mut() {
+        let num_jokers = hand.cards.iter().filter(|c| c.0 == 1).count();
+
+        if num_jokers > 0 {
+            match hand.t {
+                Type::OnePair(c) if num_jokers == 1 => {
+                    hand.t = Type::ThreeOfAKind(c);
+                }
+                Type::ThreeOfAKind(c) if c.0 == 1 && num_jokers == 3 => {
+                    // already a joker
+                }
+                Type::ThreeOfAKind(c) if num_jokers == 1 => {
+                    hand.t = Type::FourOfAKind(c);
+                }
+                Type::FullHouse { three: c1, two: c2 } if c1.0 == 1 || c2.0 == 1 => {
+                    hand.t = Type::FiveOfAKind(c2);
+                }
+                Type::FourOfAKind(c) if num_jokers == 1 => {
+                    hand.t = Type::FiveOfAKind(c);
+                }
+                Type::FourOfAKind(c) if num_jokers == 4 => {
+                    hand.t = Type::FiveOfAKind(c);
+                }
+                Type::TwoPair(c1, _c2) if num_jokers == 2 => {
+                    hand.t = Type::FourOfAKind(c1);
+                }
+                Type::TwoPair(c1, c2) if num_jokers == 1 => {
+                    hand.t = Type::FullHouse { three: c1, two: c2 };
+                }
+                Type::OnePair(c) if c.0 == 1 && num_jokers == 2 => {
+                    // already a joker
+                }
+                Type::FiveOfAKind(_c) if num_jokers == 5 => {
+                    // already a joker
+                }
+                Type::HighCard if num_jokers == 1 => {
+                    hand.t = Type::OnePair(Card(1));
+                }
+                t => {
+                    if num_jokers != 0 {
+                        dbg! {(t, num_jokers)};
+                        // unreachable!()
+                    }
+                }
+            }
+        }
+    }
+
+    hands.sort();
+    // dbg! {&hands};
+
+    Ok(hands
+        .iter()
+        .enumerate()
+        .map(|(rank, h)| (rank + 1) * h.bid)
+        .sum())
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Card(usize);
+
+#[derive(Debug)]
+struct Hand {
+    cards: Vec<Card>,
+    bid: usize,
+    t: Type,
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Hand {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+impl Eq for Hand {}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let cmp = self.t.cmp(&other.t);
+        if cmp == Ordering::Equal {
+            self.cards
+                .iter()
+                .zip(&other.cards)
+                .map(|(c1, c2)| c1.0.cmp(&c2.0))
+                .find(|x| *x != Ordering::Equal)
+                .unwrap()
+        } else {
+            cmp
+        }
+    }
+}
+
+impl Hand {
+    fn calculate_type(cards: &[Card]) -> Type {
+        let mut h: HashMap<Card, usize> = HashMap::new();
+        cards.iter().for_each(|c| *h.entry(*c).or_default() += 1);
+
+        // FiveOfAKind
+        if let Some(c) = h
+            .iter()
+            .find_map(|(c, count)| if *count == 5 { Some(*c) } else { None })
+        {
+            Type::FiveOfAKind(c)
+        } else if let Some(c) = h
+            .iter()
+            .find_map(|(c, count)| if *count == 4 { Some(*c) } else { None })
+        {
+            Type::FourOfAKind(c)
+        } else if let (Some(c1), Some(c2)) = (
+            h.iter()
+                .find_map(|(c, count)| if *count == 3 { Some(*c) } else { None }),
+            h.iter()
+                .find_map(|(c, count)| if *count == 2 { Some(*c) } else { None }),
+        ) {
+            Type::FullHouse { three: c1, two: c2 }
+        } else if let Some(c) = h
+            .iter()
+            .find_map(|(c, count)| if *count == 3 { Some(*c) } else { None })
+        {
+            Type::ThreeOfAKind(c)
+        } else {
+            let pairs: Vec<_> = h
+                .iter()
+                .filter_map(|(c, count)| if *count == 2 { Some(*c) } else { None })
+                .collect();
+            if pairs.len() == 2 {
+                Type::TwoPair(pairs[0], pairs[1])
+            } else if pairs.len() == 1 {
+                Type::OnePair(pairs[0])
+            } else {
+                Type::HighCard
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Type {
+    FiveOfAKind(Card),
+    FourOfAKind(Card),
+    FullHouse { three: Card, two: Card },
+    ThreeOfAKind(Card),
+    TwoPair(Card, Card),
+    OnePair(Card),
+    HighCard,
+}
+
+impl Type {
+    fn value(&self) -> usize {
+        match self {
+            Type::FiveOfAKind(_) => 6,
+            Type::FourOfAKind(_) => 5,
+            Type::FullHouse { .. } => 4,
+            Type::ThreeOfAKind(_) => 3,
+            Type::TwoPair(_, _) => 2,
+            Type::OnePair(_) => 1,
+            Type::HighCard => 0,
+        }
+    }
+}
+
+impl std::cmp::PartialOrd for Type {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl std::cmp::Ord for Type {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.value().cmp(&other.value())
+    }
+}
+
+fn parse_hand(i: &str) -> Hand {
+    let mut hb = i.split(' ');
+    let cards: Vec<Card> = hb
+        .next()
+        .unwrap()
+        .chars()
+        .map(|c| match c {
+            '2' => Card(2),
+            '3' => Card(3),
+            '4' => Card(4),
+            '5' => Card(5),
+            '6' => Card(6),
+            '7' => Card(7),
+            '8' => Card(8),
+            '9' => Card(9),
+            'T' => Card(10),
+            'J' => Card(11),
+            'Q' => Card(12),
+            'K' => Card(13),
+            'A' => Card(14),
+            _ => unreachable!(),
+        })
+        .collect();
+
+    let bid = hb.next().unwrap().parse().unwrap();
+    let t = Hand::calculate_type(&cards);
+
+    Hand { cards, bid, t }
+}
+
+fn parse(i: &str) -> Vec<Hand> {
+    i.lines().map(parse_hand).collect()
 }
 
 #[cfg(test)]
 mod tests {
+    const INPUT: &str = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+
     #[test]
     fn day7_run_1() {
+        assert_eq!(super::run_1(INPUT).unwrap(), 6440);
     }
 
     #[test]
     fn day7_run_2() {
+        assert_eq!(super::run_2(INPUT).unwrap(), 5905);
     }
 }
--- src/day8.rs
+++ src/day8.rs
@@ -1,3 +1,12 @@
+use std::collections::HashMap;
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::newline,
+    multi::{many1, separated_list1},
+    sequence::{preceded, separated_pair, terminated},
+};
+
 pub fn run() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("day8.txt")?;
 
@@ -7,21 +16,168 @@ pub fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn run_1(_input: &str) -> anyhow::Result<usize> {
-  todo!()
+fn run_1(input: &str) -> anyhow::Result<usize> {
+    let (_i, document) = parse(input).map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+    let mut cur = "AAA";
+    let mut iterations = 0;
+    for i in document.instructions.iter().cycle() {
+        if cur == "ZZZ" {
+            break;
+        }
+        iterations += 1;
+        let possibles = document.network.get(cur).unwrap();
+        cur = match i {
+            Instruction::Left => &possibles.0,
+            Instruction::Right => &possibles.1,
+        }
+    }
+    Ok(iterations)
 }
 
-fn run_2(_input: &str) -> anyhow::Result<usize> {
-  todo!()
+fn run_2(input: &str) -> anyhow::Result<usize> {
+    let (_i, document) = parse(input).map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+    let mut cur: Vec<String> = document
+        .network
+        .keys()
+        .filter_map(|n| {
+            if n.ends_with('A') {
+                Some(n.to_string())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let mut res = Vec::with_capacity(cur.len());
+    for c in cur.iter_mut() {
+        let mut iterations = 0;
+        for i in document.instructions.iter().cycle() {
+            if c.ends_with('Z') {
+                break;
+            }
+            iterations += 1;
+            let possibles = document.network.get(c).unwrap();
+            match i {
+                Instruction::Left => *c = possibles.0.clone(),
+                Instruction::Right => *c = possibles.1.clone(),
+            };
+        }
+        res.push(iterations);
+    }
+
+    Ok(lcm(&res))
+}
+
+pub fn lcm(nums: &[usize]) -> usize {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    let a = nums[0];
+    let b = lcm(&nums[1..]);
+    a * b / gcd_of_two_numbers(a, b)
+}
+
+fn gcd_of_two_numbers(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+    gcd_of_two_numbers(b, a % b)
+}
+
+#[derive(Debug)]
+enum Instruction {
+    Left,
+    Right,
+}
+
+#[derive(Debug)]
+struct Document {
+    instructions: Vec<Instruction>,
+    network: HashMap<String, (String, String)>,
+}
+
+fn parse_network(i: crate::Input) -> crate::PResult<(String, (String, String))> {
+    let parse_str = |i| {
+        nom::combinator::map(nom::character::complete::alphanumeric1, |c: &str| {
+            c.to_string()
+        })(i)
+    };
+
+    let res = separated_pair(
+        parse_str,
+        tag(" = "),
+        separated_pair(
+            preceded(tag("("), parse_str),
+            tag(", "),
+            terminated(parse_str, tag(")")),
+        ),
+    )(i)?;
+
+    Ok(res)
+}
+
+fn parse(i: crate::Input) -> crate::PResult<Document> {
+    let left = nom::combinator::map(tag("L"), |_| Instruction::Left);
+    let right = nom::combinator::map(tag("R"), |_| Instruction::Right);
+    let (i, instructions) = many1(nom::branch::alt((left, right)))(i)?;
+
+    let (i, _) = newline(i)?;
+    let (i, _) = newline(i)?;
+
+    let (i, network) = nom::combinator::map(separated_list1(newline, parse_network), |v| {
+        v.iter()
+            .map(|(a, (b, c))| (a.to_string(), (b.to_string(), c.to_string())))
+            .collect::<HashMap<_, _>>()
+    })(i)?;
+
+    Ok((
+        i,
+        Document {
+            instructions,
+            network,
+        },
+    ))
 }
 
 #[cfg(test)]
 mod tests {
+    const INPUT_1: &str = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+
+    const INPUT_2: &str = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+
     #[test]
     fn day8_run_1() {
+        assert_eq!(super::run_1(INPUT_1).unwrap(), 2);
+        assert_eq!(super::run_1(INPUT_2).unwrap(), 6);
     }
+
+    const INPUT_3: &str = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
 
     #[test]
     fn day8_run_2() {
+        assert_eq!(super::run_2(INPUT_3).unwrap(), 6);
     }
 }
