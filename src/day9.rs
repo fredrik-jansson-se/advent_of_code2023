28a6923193ca33ccda357bd64ea4a17babd5fe06
Fredrik Jansson <fj@avassa.io>
2023-12-10 21:28:40 +0100

    Day 9.

--- src/day9.rs
+++ src/day9.rs
@@ -7,21 +7,65 @@ pub fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn run_1(_input: &str) -> anyhow::Result<usize> {
-  todo!()
+fn calc_next(i: &[isize]) -> Vec<isize> {
+    i.windows(2).map(|a| a[1] - a[0]).collect()
 }
 
-fn run_2(_input: &str) -> anyhow::Result<usize> {
-  todo!()
+fn find_bottom(i: &[isize]) -> isize {
+    if i.iter().all(|v| *v == 0) {
+        return 0;
+    }
+
+    let n = calc_next(i);
+
+    let last = find_bottom(&n);
+    return last + i.last().unwrap();
+}
+
+fn run_1(input: &str) -> anyhow::Result<isize> {
+    let histories: Vec<Vec<_>> = input
+        .lines()
+        .map(|line| {
+            line.split(' ')
+                .map(|c| c.parse::<isize>().unwrap())
+                .collect()
+        })
+        .collect();
+    Ok(histories
+        .iter()
+        .map(|v| find_bottom(v))
+        .sum())
+}
+
+fn run_2(input: &str) -> anyhow::Result<isize> {
+    let histories: Vec<Vec<_>> = input
+        .lines()
+        .map(|line| {
+            line.split(' ')
+                .map(|c| c.parse::<isize>().unwrap())
+                .rev()
+                .collect()
+        })
+        .collect();
+    Ok(histories
+        .iter()
+        .map(|v| find_bottom(v))
+        .sum())
 }
 
 #[cfg(test)]
 mod tests {
+    const INPUT: &str = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+
     #[test]
     fn day9_run_1() {
+        assert_eq!(super::run_1(INPUT).unwrap(), 114);
     }
 
     #[test]
     fn day9_run_2() {
+        assert_eq!(super::run_2(INPUT).unwrap(), 2);
     }
 }
