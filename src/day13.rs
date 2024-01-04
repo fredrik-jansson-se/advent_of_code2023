2263b4bc6d807ffa2de0fd9ec390e53d22070af7
Fredrik Jansson <fj@avassa.io>
2024-01-05 00:15:05 +0100

    Day 14 - 1

--- src/day13.rs
+++ src/day13.rs
@@ -1,3 +1,5 @@
+// use ndarray::{ArrayViewD, IxDyn};
+
 pub fn run() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("day13.txt")?;
 
@@ -7,21 +9,143 @@ pub fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn run_1(_input: &str) -> anyhow::Result<usize> {
-  todo!()
+// 47527 -- too high
+// 47527 -- too high
+// 20074 -- too low
+// 29463 -- wrong
+fn run_1(input: &str) -> anyhow::Result<usize> {
+    let mut maps: Vec<ndarray::ArrayD<usize>> = Vec::new();
+    let mut row_maps = Vec::new();
+    let mut col_maps: Vec<Vec<usize>> = Vec::new();
+    row_maps.push(Vec::new());
+    col_maps.push(Vec::new());
+    maps.push(ndarray::ArrayD::default(ndarray::IxDyn(&[0, 1])));
+    for line in input.lines() {
+        if line.is_empty() {
+            maps.push(ndarray::ArrayD::default(ndarray::IxDyn(&[0, 1])));
+            row_maps.push(Vec::new());
+            col_maps.push(Vec::new());
+            continue;
+        }
+        // let l = maps.len();
+        // let d = maps[maps.len() - 1].dim();
+        // dbg!{&d};
+        // dbg!{&maps[l-1]};
+        // let view_dim: IxDyn = IxDyn(&[1, d[1]]);
+        // maps[l - 1].push(
+        //     ndarray::Axis(0),
+        //     ArrayViewD::from_shape(view_dim, vec![0usize; d[1]].as_slice())?,
+        // )?;
+        let mut val = 0;
+        for (col, c) in line.chars().enumerate() {
+            if col_maps[col_maps.len() - 1].get(col).is_none() {
+                let l = col_maps.len();
+                col_maps[l - 1].push(0);
+            }
+
+            if c == '#' {
+                val = (val << 1) + 1;
+                let l = col_maps.len();
+                let c_val = &mut col_maps[l - 1][col];
+                *c_val = (*c_val << 1) + 1;
+            } else {
+                val = (val << 1) + 0;
+                let l = col_maps.len();
+                let c_val = &mut col_maps[l - 1][col];
+                *c_val = (*c_val << 1) + 0;
+            }
+        }
+
+        let l = row_maps.len();
+        row_maps[l - 1].push(val);
+    }
+
+    assert_eq!(row_maps.len(), col_maps.len());
+
+    let mut res = 0;
+    let s = 0;
+    for map in col_maps.iter().skip(s) {
+        dbg! {map};
+        for check_row in 1..(map.len() - 1) {
+            let mut above_row = check_row;
+            let mut below_row = check_row + 1;
+            loop {
+                match (map.get(above_row), map.get(below_row)) {
+                    (Some(a), Some(b)) if a != b => {
+                        break;
+                    }
+                    (Some(_), Some(_)) => {
+                        //
+                    }
+                    _ => {
+                        // we've come to an end
+                        println!("Found col {check_row}");
+                        res += check_row + 1;
+                        break;
+                    }
+                }
+                above_row = above_row.wrapping_sub(1);
+                below_row += 1;
+            }
+        }
+    }
+
+    for map in row_maps.iter().skip(s) {
+        dbg! {map};
+        for check_row in 1..(map.len() - 1) {
+            let mut above_row = check_row;
+            let mut below_row = check_row + 1;
+            loop {
+                match (map.get(above_row), map.get(below_row)) {
+                    (Some(a), Some(b)) if a != b => {
+                        break;
+                    }
+                    (Some(_), Some(_)) => {
+                        //
+                    }
+                    _ => {
+                        // we've come to an end
+                        println!("Found row {check_row}");
+                        res += 100 * (check_row + 1);
+                        break;
+                    }
+                }
+                above_row = above_row.wrapping_sub(1);
+                below_row += 1;
+            }
+        }
+    }
+
+    Ok(res)
 }
 
 fn run_2(_input: &str) -> anyhow::Result<usize> {
-  todo!()
+    todo!()
 }
 
 #[cfg(test)]
 mod tests {
+    const INPUT: &str = "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
+
     #[test]
     fn day13_run_1() {
+        assert_eq!(super::run_1(INPUT).unwrap(), 405);
     }
 
     #[test]
-    fn day13_run_2() {
-    }
+    fn day13_run_2() {}
 }
--- src/day14.rs
+++ src/day14.rs
@@ -7,21 +7,34 @@ pub fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-#[derive(PartialEq, Clone)]
-enum Obj {
-    Rock,
-    Stone,
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+enum Rock {
+    Rounded,
+    Cubic,
 }
+
+// fn print_board(board: &[Vec<Option<Rock>>]) {
+//     println!("    0123456789");
+//     board.iter().enumerate().for_each(|(row_num, row)| {
+//         print!("{row_num:2}  ");
+//         row.iter().for_each(|o| match o {
+//             Some(Rock::Rounded) => print!("O"),
+//             Some(Rock::Cubic) => print!("#"),
+//             _ => print!("."),
+//         });
+//         println!();
+//     });
+// }
 
 // 107003 -- too low
 fn run_1(input: &str) -> anyhow::Result<usize> {
-    let board: Vec<Vec<Option<Obj>>> = input
+    let board: Vec<Vec<Option<Rock>>> = input
         .lines()
         .map(|line| {
             line.chars()
                 .map(|c| match c {
-                    '#' => Some(Obj::Rock),
-                    'O' => Some(Obj::Stone),
+                    '#' => Some(Rock::Cubic),
+                    'O' => Some(Rock::Rounded),
                     _ => None,
                 })
                 .collect()
@@ -30,67 +43,174 @@ fn run_1(input: &str) -> anyhow::Result<usize> {
 
     let mut new_board = board.clone();
     for cur_row in 0..board.len() {
-        for (c, o) in board[cur_row].iter().enumerate() {
+        let borrowed_row = new_board[cur_row].clone();
+        for (c, o) in borrowed_row.iter().enumerate() {
             // Check below to find ball
             if o.is_none() {
                 for row in (cur_row + 1)..board.len() {
-                    if Some(Obj::Stone) == board[row][c] {
+                    if Some(Rock::Rounded) == new_board[row][c] {
                         new_board[cur_row][c] = new_board[row][c].take();
                         break;
-                    } else if Some(Obj::Rock) == board[row][c] {
+                    } else if Some(Rock::Cubic) == board[row][c] {
                         break;
                     }
                 }
             }
         }
     }
-    // board.iter().for_each(|row| {
-    //     row.iter().for_each(|o| match o {
-    //         Some(Obj::Stone) => print!("O"),
-    //         Some(Obj::Rock) => print!("#"),
-    //         _ => print!(" "),
-    //     });
-    //     println!();
-    // });
-    // println!();
-    // new_board.iter().for_each(|row| {
-    //     row.iter().for_each(|o| match o {
-    //         Some(Obj::Stone) => print!("O"),
-    //         Some(Obj::Rock) => print!("#"),
-    //         _ => print!(" "),
-    //     });
-    //     println!();
-    // });
 
     Ok(new_board
         .iter()
         .rev()
         .enumerate()
-        .map(|(r_num, row)| row.iter().filter(|o| *o == &Some(Obj::Stone)).count() * (r_num + 1))
+        .map(|(r_num, row)| row.iter().filter(|o| *o == &Some(Rock::Rounded)).count() * (r_num + 1))
         .sum())
 }
 
-fn run_2(_input: &str) -> anyhow::Result<usize> {
-    todo!()
+fn run_cycle(board: &mut [Vec<Option<Rock>>]) {
+    // North
+    for cur_row in 0..board.len() {
+        let borrowed_row = board[cur_row].clone();
+        for (c, o) in borrowed_row.iter().enumerate() {
+            // Check below to find ball
+            if o.is_none() {
+                for row in (cur_row + 1)..board.len() {
+                    if Some(Rock::Rounded) == board[row][c] {
+                        board[cur_row][c] = board[row][c].take();
+                        break;
+                    } else if Some(Rock::Cubic) == board[row][c] {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // println!("Before West");
+    // print_board(&board);
+
+    // West
+    for cur_row in 0..board.len() {
+        // Check east to find ball
+        for col in 0..(board[cur_row].len() - 1) {
+            let o = &board[cur_row][col];
+            if o.is_none() {
+                for check_col in (col + 1)..board[cur_row].len() {
+                    if Some(Rock::Rounded) == board[cur_row][check_col] {
+                        board[cur_row][col] = board[cur_row][check_col].take();
+                        break;
+                    } else if Some(Rock::Cubic) == board[cur_row][check_col] {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // println!("Before South");
+    // print_board(&board);
+
+    // South
+    for cur_row in (1..board.len()).rev() {
+        let borrowed_row = board[cur_row].clone();
+        for (c, o) in borrowed_row.iter().enumerate() {
+            // Check above to find ball
+            if o.is_none() {
+                for row in (0..cur_row).rev() {
+                    if Some(Rock::Rounded) == board[row][c] {
+                        board[cur_row][c] = board[row][c].take();
+                        break;
+                    } else if Some(Rock::Cubic) == board[row][c] {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    // println!("Before East");
+    // print_board(&board);
+
+    // East
+    for cur_row in 0..board.len() {
+        // Check east to find ball
+        for col in (1..(board[cur_row].len())).rev() {
+            let o = &board[cur_row][col];
+            if o.is_none() {
+                for check_col in (0..col).rev() {
+                    // println!("col: {col} check: {check_col}");
+                    if Some(Rock::Rounded) == board[cur_row][check_col] {
+                        board[cur_row][col] = board[cur_row][check_col].take();
+                        break;
+                    } else if Some(Rock::Cubic) == board[cur_row][check_col] {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+// too high: 94969
+// wrong: 93740
+// too low: 93718
+// too low: 93694
+
+fn run_2(input: &str) -> anyhow::Result<usize> {
+    let mut board: Vec<Vec<Option<Rock>>> = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| match c {
+                    '#' => Some(Rock::Cubic),
+                    'O' => Some(Rock::Rounded),
+                    _ => None,
+                })
+                .collect()
+        })
+        .collect();
+
+    let mut prev = vec![];
+    let dest = 1_000_000_000;
+    for i in 1..=dest {
+        run_cycle(&mut board);
+        if !prev.contains(&board) {
+            prev.push(board.clone());
+        } else {
+            if (i % prev.len()) == (dest % prev.len()) {
+                println!("Break {i}");
+                println!("m: {}", dest%prev.len());
+                break;
+            }
+        }
+    }
+
+    Ok(board
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(r_num, row)| row.iter().filter(|o| *o == &Some(Rock::Rounded)).count() * (r_num + 1))
+        .sum())
 }
 
 #[cfg(test)]
 mod tests {
-    const INPUT: &str = "OOOO.#.O..
-OO..#....#
-OO..O##..O
-O..#.OO...
-........#.
-..#....#.#
-..O..#.O.O
-..O.......
+    const INPUT: &str = "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
 #....###..
-#....#....";
+#OO..#....";
+
     #[test]
     fn day14_run_1() {
         assert_eq!(super::run_1(INPUT).unwrap(), 136);
     }
 
     #[test]
-    fn day14_run_2() {}
+    fn day14_run_2() {
+        assert_eq!(super::run_2(INPUT).unwrap(), 64);
+    }
 }
